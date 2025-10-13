59973d572d94b31894f86db588ff88e5e9f18cc7
Abdul Ohab <abdulohab@Abduls-MacBook-Air.local>
2025-10-13 09:12:55 +0600

    default port changed

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    port: Number(process.env.PORT) || 3000,
+  },
 });
