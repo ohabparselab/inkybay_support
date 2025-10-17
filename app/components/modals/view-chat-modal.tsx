9c29b1729993c062cc7cb30c9a40b5842475e57a
ohabparselab <ohab@parselab.com>
2025-10-17 14:16:26 +0600

    Chat transcript view & download feature added

--- app/components/modals/view-chat-modal.tsx
+++ app/components/modals/view-chat-modal.tsx
@@ -7,7 +7,6 @@ import {
 } from "@/components/ui/dialog";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { ScrollArea } from "@/components/ui/scroll-area";
 import { format } from "date-fns";
 import { Separator } from "../ui/separator";
 
@@ -23,55 +22,80 @@ export function ViewChatDetailsModal({ open, onOpenChange, chat }: ViewChatDetai
 
     return (
         <Dialog open={open} onOpenChange={onOpenChange}>
-             <DialogContent className="max-h-[90vh] overflow-y-auto sm:max-w-5xl">
+            <DialogContent className="max-h-[90vh] overflow-y-auto sm:max-w-5xl">
                 <DialogHeader className="p-5 border-b">
                     <DialogTitle>Chat Details</DialogTitle>
                 </DialogHeader>
-                {/* <ScrollArea className="px-6"> */}
-                    <section className="space-y-5 border rounded p-3">
-                        <h3 className="text-base font-semibold mb-3">Chat Info</h3>
-                        <Separator />
-                        {/* <div className="grid grid-cols-2 gap-x-6 gap-y-4 p-4 text-sm text-gray-700 bg-white rounded-md shadow-sm"> */}
-                        <div className="grid grid-cols-2 gap-x-6 space-y-2 text-sm text-gray-700">
-                            <p><strong>Chat Date:</strong> {formatDate(chat.chatDate)}</p>
-                            <p><strong>Handled By:</strong> {chat.handleByUser?.fullName || "-"}</p>
-                            <p><strong>Chat Transcript:</strong> {chat?.chatTranscript || "N/A"}</p>
-                            <p><strong>Review Asked:</strong> {chat?.reviewAsked ? "Yes" : 'No'}</p>
-                            <p><strong>Review Status:</strong> {chat?.reviewStatus ? "Yes" : 'No'}</p>
-                            <p><strong>Agent Rating:</strong> {chat?.agentRating || "N/A"}</p>
-                            <p><strong>Other Store Url:</strong> {chat?.otherStoresUrl || "N/A"}</p>
-                            <p><strong>Last Review Approach:</strong> {formatDate(chat.chatDate) || "N/A"}</p>
-                            <p><strong>Tags:</strong>
-                                {chat.chatTags && chat.chatTags.length > 0 ? (
-                                    <>
-                                        {chat.chatTags.map((ct: any, i: number) => (
-                                            <Badge key={i} variant="secondary" className="text-xs">
-                                                {ct.tag.name}
-                                            </Badge>
-                                        ))}
-                                    </>
-                                ) : (
-                                    <span className="text-gray-500">N/A</span>
-                                )}
-                            </p>
-                            <p><strong>Store Details:</strong> {chat?.storeDetails || "N/A"}</p>
-                            <p><strong>Created At:</strong> {formatDate(chat.createdAt)}</p>
-                            <p><strong>Updated At:</strong> {formatDate(chat.updatedAt)}</p>
-                            <p><strong>Review Text:</strong> {chat?.reviewText || "N/A"}</p>
-                            <p><strong>Client Query:</strong> {chat.clientQuery || "-"}</p>
-                            <p><strong>Client Feedback:</strong> {chat?.clientFeedback || "N/A"}</p>
-                            <p><strong>Feature Request:</strong> {chat?.featureRequest || "N/A"}</p>
-                            <p><strong>Agent Comment:</strong> {chat?.agentComments || "N/A"}</p>
-                            <p><strong>Changes Made By Agent:</strong> {chat?.changesMadeByAgent || "N/A"}</p>
-                            
-                        </div>
-                        <Separator />
-                        <section>
-                            <h3 className="text-base font-semibold mb-3">Shop Details</h3>
-                            <p className="text-gray-500 italic">Coming soon...</p>
-                        </section>
+                <section className="space-y-5 border rounded p-3">
+                    <h3 className="text-base font-semibold mb-3">Chat Info</h3>
+                    <Separator />
+                    <div className="grid grid-cols-2 gap-x-6 space-y-2 text-sm text-gray-700">
+                        <p><strong>Chat Date:</strong> {formatDate(chat.chatDate)}</p>
+                        <p><strong>Handled By:</strong> {chat.handleByUser?.fullName || "-"}</p>
+                        <p className="break-words">
+                           
+                            {chat.chatTranscript ? (
+                                <div className="flex gap-2 mt-1">
+                                     <strong>Chat Transcript:</strong>{" "}
+                                    {/* View Button */}
+                                    <a
+                                        href={chat.chatTranscript}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="text-blue-600 hover:underline"
+                                    >
+                                       View
+                                    </a>
+
+                                    {/* Download Button */}
+                                    <a
+                                        href={chat.chatTranscript}
+                                        download
+                                        className="bg-blue-100 text-blue-700 px-2 py-1 rounded hover:bg-blue-200 text-xs"
+                                    >
+                                        Download
+                                    </a>
+                                </div>
+                            ) : (
+                                <span className="text-gray-500">N/A</span>
+                            )}
+                        </p>
+
+                        <p><strong>Review Asked:</strong> {chat?.reviewAsked ? "Yes" : 'No'}</p>
+                        <p><strong>Review Status:</strong> {chat?.reviewStatus ? "Yes" : 'No'}</p>
+                        <p><strong>Agent Rating:</strong> {chat?.agentRating || "N/A"}</p>
+                        <p><strong>Other Store Url:</strong> {chat?.otherStoresUrl || "N/A"}</p>
+                        <p><strong>Last Review Approach:</strong> {formatDate(chat.chatDate) || "N/A"}</p>
+                        <p><strong>Tags:</strong>
+                            {chat.chatTags && chat.chatTags.length > 0 ? (
+                                <>
+                                    {chat.chatTags.map((ct: any, i: number) => (
+                                        <Badge key={i} variant="secondary" className="text-xs">
+                                            {ct.tag.name}
+                                        </Badge>
+                                    ))}
+                                </>
+                            ) : (
+                                <span className="text-gray-500">N/A</span>
+                            )}
+                        </p>
+                        <p><strong>Store Details:</strong> {chat?.storeDetails || "N/A"}</p>
+                        <p><strong>Created At:</strong> {formatDate(chat.createdAt)}</p>
+                        <p><strong>Updated At:</strong> {formatDate(chat.updatedAt)}</p>
+                        <p><strong>Review Text:</strong> {chat?.reviewText || "N/A"}</p>
+                        <p><strong>Client Query:</strong> {chat.clientQuery || "-"}</p>
+                        <p><strong>Client Feedback:</strong> {chat?.clientFeedback || "N/A"}</p>
+                        <p><strong>Feature Request:</strong> {chat?.featureRequest || "N/A"}</p>
+                        <p><strong>Agent Comment:</strong> {chat?.agentComments || "N/A"}</p>
+                        <p><strong>Changes Made By Agent:</strong> {chat?.changesMadeByAgent || "N/A"}</p>
+
+                    </div>
+                    <Separator />
+                    <section>
+                        <h3 className="text-base font-semibold mb-3">Shop Details</h3>
+                        <p className="text-gray-500 italic">Coming soon...</p>
                     </section>
-                {/* </ScrollArea> */}
+                </section>
                 <DialogFooter className="p-6 border-t">
                     <Button variant="outline" onClick={() => onOpenChange(false)}>Close</Button>
                 </DialogFooter>
