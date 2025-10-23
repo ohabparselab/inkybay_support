2db30f98828dd0b66060f17f3b6d59a3dc7d14bb
ohabparselab <ohab@parselab.com>
2025-10-23 14:48:17 +0600

    vite config change

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,24 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const port =
+    process.env.PARSETRACK_NODE_ENV === "beta"
+        ? process.env.PARSETRACK_BETA_PORT
+        : process.env.PARSETRACK_LIVE_PORT;
 
-const PORT = process.env.PARSETRACK_NODE_ENV == "production" ? process.env.PARSETRACK_LIVE_PORT : process.env.PARSETRACK_LIVE_PORT;
-console.log("=======port=======>>", PORT);
+const serverPort = Number(port) || 5001; // fallback if undefined
+
+console.log("=======port=======>>", serverPort);
+
 export default defineConfig({
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
     server: {
-        port: Number(PORT) || 3000,
+        port: serverPort,
         allowedHosts: ["otelia-unbrightened-sully.ngrok-free.dev", "parsetrack.com"],
         hmr: { overlay: true },
     },
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
     build: {
         sourcemap: process.env.PARSETRACK_NODE_ENV !== "production",
     },
 });
+
