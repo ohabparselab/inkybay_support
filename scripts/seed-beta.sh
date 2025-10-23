168eea2e93e1ddca51028f24d5929e7d0c0d40f4
ohabparselab <ohab@parselab.com>
2025-10-23 16:27:21 +0600

    update seed file

--- scripts/seed-beta.sh
+++ scripts/seed-beta.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source /etc/environment
+export PATH=$PATH:$(npm bin)
 export DATABASE_URL=$PARSETRACK_BETA_DATABASE_URL
 export PARSETRACK_NODE_ENV=beta
 
-ts-node prisma/seed.ts
+npx ts-node prisma/seed.ts
