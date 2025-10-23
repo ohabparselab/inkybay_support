b78ed6016948ddbdb4883d3d2fc0480ff4fe1165
ohabparselab <ohab@parselab.com>
2025-10-23 17:04:06 +0600

    start beta .sh file updated

--- scripts/start-beta.sh
+++ scripts/start-beta.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+
+# Load OS environment variables
 source /etc/environment
+
+# Make local node_modules/.bin available
+export PATH=$PATH:$(npm bin)
+
+# Set environment
 export PARSETRACK_NODE_ENV=beta
 
 # Pick the correct port
 PORT=${PARSETRACK_BETA_PORT:-5001}
 export PORT
 
-# Start Remix server
-react-router-serve ./build/server/index.js
+# Start Remix server using local binary
+npx react-router-serve ./build/server/index.js
