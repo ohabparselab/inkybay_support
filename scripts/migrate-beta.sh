260f70c5c8fdabe749c62db6ed9784391bdd2e46
ohabparselab <ohab@parselab.com>
2025-11-13 16:34:39 +0600

    beta final script updated

--- scripts/migrate-beta.sh
+++ scripts/migrate-beta.sh
@@ -2,5 +2,4 @@
 source /etc/environment
 export DATABASE_URL=$PARSETRACK_BETA_DATABASE_URL
 export PARSETRACK_NODE_ENV=beta
-npx prisma migrate reset --force
-npx prisma generate
+npx prisma migrate deploy
