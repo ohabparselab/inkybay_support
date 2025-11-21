8905c4fbd9b84ad52acec2d5351291a9ba56f600
ohabparselab <ohab@parselab.com>
2025-11-21 13:44:04 +0600

    server log check date filtering

--- app/routes/chats/list.tsx
+++ app/routes/chats/list.tsx
@@ -83,21 +83,46 @@ export async function loader({ request }: LoaderFunctionArgs) {
     };
 
     if (date) {
-    const d = new Date(date);
+        const d = new Date(date);
 
-    const start = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), 0, 0, 0, 0));
-    const end   = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate(), 23, 59, 59, 999));
+        const start = new Date(
+            d.getFullYear(),
+            d.getMonth(),
+            d.getDate(),
+            0, 0, 0, 0
+        );
 
-    where.chatDate = { gte: start, lt: end };
-} else if (startDate && endDate) {
-    const s = new Date(startDate);
-    const e = new Date(endDate);
+        const end = new Date(
+            d.getFullYear(),
+            d.getMonth(),
+            d.getDate(),
+            23, 59, 59, 999
+        );
 
-    const start = new Date(Date.UTC(s.getFullYear(), s.getMonth(), s.getDate(), 0, 0, 0, 0));
-    const end   = new Date(Date.UTC(e.getFullYear(), e.getMonth(), e.getDate(), 23, 59, 59, 999));
+        where.chatDate = {
+            gte: start,
+            lt: end,
+        };
+    } else if (startDate && endDate) {
+        const s = new Date(startDate);
+        const e = new Date(endDate);
 
-    where.chatDate = { gte: start, lt: end };
-}
+        const start = new Date(
+            s.getFullYear(), s.getMonth(), s.getDate(),
+            0, 0, 0, 0
+        );
+
+        const end = new Date(
+            e.getFullYear(), e.getMonth(), e.getDate(),
+            23, 59, 59, 999
+        );
+
+        where.chatDate = {
+            gte: start,
+            lt: end,
+        };
+    }
+    
     console.log(where)
 
     const [chats, total, tags] = await Promise.all([
