0077bb0fb93f5a55665f7d51e80ab212cd52abcc
ohabparselab <ohab@parselab.com>
2025-12-03 17:35:22 +0600

    update migrate script

--- prisma/migrations/20251203064131_update_chats_reviews_table/migration.sql
+++ prisma/migrations/20251203064131_update_chats_reviews_table/migration.sql
@@ -5,16 +5,16 @@
 
 */
 -- DropForeignKey
-ALTER TABLE `chats` DROP FOREIGN KEY `chats_handleBy_fkey`;
+-- ALTER TABLE `chats` DROP FOREIGN KEY `chats_handleBy_fkey`;
 
 -- DropForeignKey
-ALTER TABLE `reviews` DROP FOREIGN KEY `reviews_reviewApproachBy_fkey`;
+-- ALTER TABLE `reviews` DROP FOREIGN KEY `reviews_reviewApproachBy_fkey`;
 
 -- DropIndex
-DROP INDEX `chats_handleBy_fkey` ON `chats`;
+-- DROP INDEX `chats_handleBy_fkey` ON `chats`;
 
 -- DropIndex
-DROP INDEX `reviews_reviewApproachBy_fkey` ON `reviews`;
+-- DROP INDEX `reviews_reviewApproachBy_fkey` ON `reviews`;
 
 -- AlterTable
 ALTER TABLE `chats` DROP COLUMN `handleBy`;
