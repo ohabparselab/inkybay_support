2dcc334d44df691bf53dc07de26db7ff88f9d237
ohabparselab <ohab@parselab.com>
2025-12-03 17:00:11 +0600

    migration final script push

--- scripts/migrate-beta.sh
+++ scripts/migrate-beta.sh
@@ -2,6 +2,5 @@
 source /etc/environment
 export DATABASE_URL=$PARSETRACK_BETA_DATABASE_URL
 export PARSETRACK_NODE_ENV=beta
-npx prisma migrate resolve --applied 20251203092611_update_meeting_table
 npx prisma migrate deploy
 npx prisma generate
